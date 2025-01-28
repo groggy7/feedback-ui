26ae5672815e7bf31f7cd8d351201b8be19ba18d
Serhat Kılbaş <serhatkilbas127@gmail.com>
2025-01-28 19:41:21 +0300

    Add header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
+import Header from "./components/Header"
+
 function App() {
   return (
-    <div className="text-2xl text-center">Hello World!</div>
+    <Header />
   )
 }
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,5 @@
+export default function Header() {
+  return <div className="uppercase text-2xl text-yellow-600 text-center font-semibold">
+        Feedback UI
+    </div>
+}
